8b8e1a6b19a143f24c4bde2a20506b5baf3f1287
Aldaaairr <aldaair10@gmail.com>
2022-06-11 21:11:55 -0500

    asd

---
+++ models/Producto.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductoSchema = new Schema({
+    nombre: {type: String, required: true, max: 50},
+    codigo: {type: String, required: true, max: 20},
+    stock: {type: String, required: true, max: 20},
+    precio: {type: String, required: true, max: 20},
+    proovedor: {type: String, required: true, max: 50},
+});
+module.exports = mongoose.model('Producto', ProductoSchema);
